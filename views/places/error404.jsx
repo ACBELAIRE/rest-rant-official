972d72d9286da45ef3c578b60d4bc8805b432544
Your Name <you@example.com>
2023-02-07 23:35:04 -0800

    made places index page

---
+++ views/places/error404.jsx
@@ -0,0 +1,14 @@
+const React = require('react')
+const Def = require('./default')
+
+const error404Places(){
+    return(
+        <Def>
+            <main>
+                <h1>404: PAGE NOT FOUND</h1>
+                <p>Oops, sorry, we can't find this place!</p>
+            </main>
+        </Def>
+    )
+}
+module.exports = error404Places
---
+++ views/places/home.jsx
@@ -0,0 +1,15 @@
+const React = require('react')
+const Def = require('./default')
+
+const homePlaces(){
+    return(
+        <Def>
+            <main>
+                <h1> Places Home</h1>
+            </main>
+
+        </Def>
+
+    )
+}
+module.exports = homePlaces
