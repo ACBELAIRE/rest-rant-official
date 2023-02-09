4d28f7d2c1ed92e43683b7941ae35c700d6c0fe3
Your Name <you@example.com>
2023-02-08 22:39:39 -0800

    add image to 404 page

--- views/error404.jsx
+++ views/error404.jsx
@@ -7,6 +7,12 @@ function error404 () {
             <main>
                 <h1>404: PAGE NOT FOUND</h1>
                 <p>Oops, sorry, we can't find this page!</p>
+                <div>
+                        <img src="/images/childicecream.jpg" alt="Photo of a childs hand holding a ice cream cone with two scoops of ice cream" />
+                    <div>
+                        Photo by <a href="https://unsplash.com/@patrickian4?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Patrick Fore</a> on <a href="https://unsplash.com/photos/AfhSPYdkxiU?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                    </div>
+                </div>
             </main>
         </Def>
     )
--- views/home.jsx
+++ views/home.jsx
@@ -6,10 +6,15 @@ function home() {
     <Def>
         <main>
             <h1>HOME</h1>
+            <div>
+                <img src="/images/pastaDish.jpg" alt="Photo of Pasta Dish on a white plate" />
+                <div>
+                    Photo by <a href="https://unsplash.com/@eaterscollective?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Eaters Collective</a> on <a href="https://unsplash.com/photos/ddZYOtZUnBk?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                </div>
+            </div>
             <a href="/places">
                 <button className="btn-primary">Places Page</button>
             </a>
-
         </main>
     </Def>
 )
