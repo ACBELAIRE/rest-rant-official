ebf114f5f7ea83cac5893a2f57ed27df95ffe502
Your Name <you@example.com>
2023-02-22 01:57:11 -0800

    adding bootstrap grid to index page

--- controllers/places.js
+++ controllers/places.js
@@ -6,31 +6,31 @@ router.get('/', (req, res) => {
             city: 'Las Vegas',
             state: 'NV',
             cuisines: 'Desserts, Bakery',
-            pic: 'https://www.mommytravels.net/wp-content/uploads/2018/11/Cafe-Hollywood.png'
+            pic: '/images/megabytes.png'
           }, {
             name: 'Back-End Devities',
             city: 'Philadelphia',
             state: 'PA',
             cuisines: 'Italian',
-            pic: 'https://www.hotelmousai.com/blog/wp-content/uploads/2021/12/Top-10-Traditional-Foods-in-Italy-1140x694.jpg'
+            pic: '/images/devities.jpg'
           }, {
             name: 'Codings Crossaints',
             city: 'New York City',
             state: 'NY',
             cuisines: 'French, Bakery',
-            pic: 'https://breadsandsweets.com/wp-content/uploads/2021/08/croissant-pic-4.jpg'
+            pic: '/images/croissants.jpg'
           },{
             name: 'Booleans Beans',
             city: 'Seattle',
             state: 'WA',
             cuisines: 'Coffee',
-            pic: 'https://cdn.luxe.digital/media/2020/05/07203610/best-coffee-beans-luxe-digital.jpg.webp'
+            pic: '/images/booleansbeans.jpg'
           },{
             name: 'Hamburger Hackers',
             city: 'Silicon Valley',
             state: 'CA',
             cuisines: 'American',
-            pic: 'https://assets.biggreenegg.eu/app/uploads/2017/12/05133526/topimage-mango-filled-hamburgers-800x500.jpg'
+            pic: '/images/hackersburgers.jpg'
           },
         ]
 
