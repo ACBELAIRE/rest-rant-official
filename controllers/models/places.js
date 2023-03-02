fd4ef192ac6f0004dbb008073ca1107712155a0b
Your Name <you@example.com>
2023-03-01 18:07:53 -0800

    New places can be added

---
+++ controllers/models/places.js
@@ -0,0 +1,32 @@
+module.exports  = [{
+    name: 'Mega-Bytes',
+    city: 'Las Vegas',
+    state: 'NV',
+    cuisines: 'Desserts, Bakery',
+    pic: '/images/megabytes.png'
+  }, {
+    name: 'Back-End Devities',
+    city: 'Philadelphia',
+    state: 'PA',
+    cuisines: 'Italian',
+    pic: '/images/devities.jpg'
+  }, {
+    name: 'Codings Crossaints',
+    city: 'New York City',
+    state: 'NY',
+    cuisines: 'French, Bakery',
+    pic: '/images/croissants.jpg'
+  },{
+    name: 'Booleans Beans',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Coffee',
+    pic: '/images/booleansbeans.jpg'
+  },{
+    name: 'Hamburger Hackers',
+    city: 'Silicon Valley',
+    state: 'CA',
+    cuisines: 'American',
+    pic: '/images/hackersburgers.jpg'
+  },
+]
--- controllers/places.js
+++ controllers/places.js
@@ -1,39 +1,7 @@
 const router = require('express').Router()
+const places = require('./models/places.js')
 
 router.get('/', (req, res) => {
-    let places = [{
-            name: 'Mega-Bytes',
-            city: 'Las Vegas',
-            state: 'NV',
-            cuisines: 'Desserts, Bakery',
-            pic: '/images/megabytes.png'
-          }, {
-            name: 'Back-End Devities',
-            city: 'Philadelphia',
-            state: 'PA',
-            cuisines: 'Italian',
-            pic: '/images/devities.jpg'
-          }, {
-            name: 'Codings Crossaints',
-            city: 'New York City',
-            state: 'NY',
-            cuisines: 'French, Bakery',
-            pic: '/images/croissants.jpg'
-          },{
-            name: 'Booleans Beans',
-            city: 'Seattle',
-            state: 'WA',
-            cuisines: 'Coffee',
-            pic: '/images/booleansbeans.jpg'
-          },{
-            name: 'Hamburger Hackers',
-            city: 'Silicon Valley',
-            state: 'CA',
-            cuisines: 'American',
-            pic: '/images/hackersburgers.jpg'
-          },
-        ]
-
     res.render('places/index',{places})
 })
 
@@ -42,9 +10,20 @@ router.get('/new', (req, res) => {
 })
 
 router.post('/', (req, res) => {
-  console.log(req.body)
-  res.send('POST /places')
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = 'https://publish.purewow.net/wp-content/uploads/sites/2/2020/12/littlebeettable-healthy-restaurants-nyc.jpg?fit=728%2C524'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
 })
+
 
 
 module.exports = router
