f0ee4795274c3bfc9a836dbd523db259b40ddd19
Your Name <you@example.com>
2023-03-10 22:20:25 -0800

    added the edit form

--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -1,12 +1,12 @@
 const React = require('react')
 const Def = require('../default')
 
-const edit_form (data) {
+function edit_form (data) {
     return(
         <Def>
                     
             <main>
-                <h1> Add a New Place</h1>
+                <h1> Edit Place</h1>
                 <form method="POST" action={`/places/${data.id}?_method=PUT`}>
                     
                     <div className='row'>
@@ -53,7 +53,7 @@ const edit_form (data) {
                                 required
                                 />
                         </div>
-                        <input className='btn btn-primary' type="submit" value="Add Place" />
+                        <input className='btn btn-primary' type="submit" value="Edit Place" />
                 </form>
             </main>
         </Def>
