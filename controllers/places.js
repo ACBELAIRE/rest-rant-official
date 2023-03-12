7037786af80f80ca628d19273d987c3aa2cd4f89
Your Name <you@example.com>
2023-03-12 01:22:24 -0800

    Custom Error Message

--- controllers/places.js
+++ controllers/places.js
@@ -19,10 +19,20 @@ router.post('/', (req, res) => {
       res.redirect('/places')
   })
   .catch(err => {
-      console.log('err', err)
+    if (err && err.name == 'ValidationError') {
+      let message = 'Validation Error: '
+      for (var field in err.errors) {
+          message += `${field} was ${err.errors[field].value}. `
+          message += `${err.errors[field].message}`
+      }
+      res.render('places/new', { message })
+  }
+  else {
       res.render('error404')
+  }
   })
 })
+
 
 
 router.get('/new', (req, res) => {
