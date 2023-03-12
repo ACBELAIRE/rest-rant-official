3e72a283894e44e4b5cddaa71d75bfb75a63c192
Your Name <you@example.com>
2023-03-12 00:53:33 -0800

    Create and Use a Helper Method

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -9,21 +9,19 @@ function show (data) {
                 <div className='col-sm-6'>
                 <br></br>
                   <img src= {data.place.pic} alt={data.place.name} width='375' height="375"/>
+                  <h3> Located in {data.place.city}, {data.place.state}</h3>
                 </div>
        
               <div className='col-sm-6'>
                 <div className='description'>
                 <br></br>
-       
                 <br></br>
                 <h1>{data.place.name}</h1>
                   <h2>Rating</h2>
                   <p>Currently unrated</p>
 
                   <h2>Description</h2>
-                  <h3> Located in {data.place.city}, {data.place.state}</h3>
-                  <h3>{data.place.showEstablished} </h3>
-
+                  <h3>{data.place.showEstablished()} </h3>
                   <h4> Serving Up  {data.place.cuisines}</h4>
                 
                   <div className='buttons'>
