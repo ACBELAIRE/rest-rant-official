db8fbc876e177ceac87c3f2138b27ab63473341f
Your Name <you@example.com>
2023-03-16 22:18:12 -0700

    Display stars for ratings

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -7,26 +7,33 @@ function show (data) {
         No Comments Yet!
       </h3>
     )
+    let rating = (
+      <h3 className="inactive">
+        Not yet rated!
+      </h3>
+    )
+    
     if (data.place.comments.length) {
-      comments = data.place.comments.map(c => {
-        return (
-          <div className="border">
-            <h2 className="rant">{c.rant ? 'Rant! ðŸ˜¡' : 'Rave! ðŸ˜»'}</h2>
-            <h4>{c.content}</h4>
-            <h3>
-              <stong>- {c.author}</stong>
-            </h3>
-            <h4>Rating: {c.stars}</h4>
-          </div>
+      let sumRatings = data.place.comments.reduce((tot, c) => {
+        return tot + c.stars
+      }, 0)
+      let averageRating = Math.round(sumRatings / data.place.comments.length)
+      let stars = ''
+      for (let i = 0; i < averageRating; i++) {
+        stars += '⭐️'
+      }
+        rating = (
+          <h3>
+            {stars} stars
+          </h3>
         )
-      })
     }
     return (
         <Def>
           <main>
             <div className='row'>
                 <div className='col-sm-6'>
-                <br></br>
+                <br/>
                   <img src= {data.place.pic} alt={data.place.name} width='375' height="375"/>
                   <h3> Located in {data.place.city}, {data.place.state}</h3>
                 </div>
@@ -36,8 +43,9 @@ function show (data) {
                 <br></br>
                 <br></br>
                 <h1>{data.place.name}</h1>
-                  <h2>Rating</h2>
-                  <p>Currently unrated</p>
+                <h2> Rating </h2>
+                  {rating}
+                  <br/>
 
                   <h2>Description</h2>
                   <h3>{data.place.showEstablished()} </h3>
