691b4645ec29e3d799ab1f90cd3e619c5d97c8a2
Your Name <you@example.com>
2023-04-07 15:21:26 -0700

    Replace local connection string with atlas one

--- index.js
+++ index.js
@@ -4,15 +4,9 @@ const express = require('express');
 const app = express();
 const methodOverride = require('method-override')
 
-
-//mongoose connection for Atlas Connection
-const mongoose = require('mongoose')
-mongoose.connect(process.env.MONGO_URI,{ 
-
-    useNewUrlParser: true
-})
-.then(() => console.log('DB connected!'))
-.catch(err => console.error(err))
+//Connect to Mongo DB
+const connectDB = require('./config/db');
+connectDB();
 
 
 // Express Settings
